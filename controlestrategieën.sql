cbb5092c381fd2c83c7a0f7655f7c3a0f0f99264
Vincent Nys <vincentnys@gmail.com>
2020-01-09 08:31:28 +0100

    Om te kunnen slagen voor opdracht.

---
+++ controlestrategieën.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE ModernWaysBL;
+USE ModernWaysBL;
+CREATE TABLE Cursussen (
+Titel VARCHAR(100) CHAR SET UTF8MB4 NOT NULL
+);
+INSERT INTO CURSUSSEN (Titel)
+VALUES
+('Programmeren'),
+('Databanken'),
+('Webtechnologie');
+`
